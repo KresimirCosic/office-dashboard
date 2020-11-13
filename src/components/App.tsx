d07a0e45e54672fec6fad6fcb22637d00d3760e3
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-11-13 08:56:04 +0100

    Initial fetch and set of data Redux store working as intended, fetched data is set correctly.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,24 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
+import { storeID } from '../constants/api';
+import { storeService } from '../services/StoreService';
+import { productsService } from '../services/ProductsService';
+import { categoriesService } from '../services/CategoriesService';
+import {
+  setStoreData,
+  setProductsData,
+  setCategoriesData,
+} from '../redux/slices/shop';
 import routes from '../routing/routes';
 import Page from './Page';
 import Navbar from './Navbar';
 import Container from './Container';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+
   // Used to trigger first transition upon loading the application for the first time
   useEffect(() => {
     const PageElement = document.getElementById('Page');
@@ -14,6 +26,19 @@ const App: React.FC = () => {
     if (PageElement) {
       PageElement.classList.add('Page-enter-done');
     }
+
+    // Fetching and setting data in Redux store upon opening the application for the first time
+    storeService.getData().then(({ data }) => {
+      dispatch(setStoreData({ id: storeID, data }));
+    });
+
+    productsService.getData().then(({ data }) => {
+      dispatch(setProductsData([...data]));
+    });
+
+    categoriesService.getData().then(({ data }) => {
+      dispatch(setCategoriesData([...data]));
+    });
   }, []);
 
   return (
