9b016a01ae4f5cca761fdf671376a20dca7ba076
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-11-15 10:15:09 +0100

    Grid view Enabled a toggle between panel and grid view.

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import ViewStreamIcon from '@material-ui/icons/ViewStream';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
 
 import { RootState } from '../redux/store';
 import Product from './Product';
 
 const ProductsList: React.FC = () => {
   const { products } = useSelector((state: RootState) => state.shop);
+  const [gridView, setGridView] = useState(false);
+
+  const handleViewChange = () => {
+    const products = document.querySelectorAll('.Product');
+    products.forEach((product) => {
+      if (product.className.split(' ').indexOf('flex-item') > -1)
+        product.classList.remove('flex-item');
+      else product.classList.add('flex-item');
+    });
+  };
 
   return (
-    <ul className='ProductsList'>
-      {products.map((product, index) => (
-        <Product key={product.id} index={index} {...product} />
-      ))}
-    </ul>
+    <>
+      <ButtonGroup variant='outlined' className='ProductsList-controls'>
+        <Button
+          onClick={() => {
+            setGridView(false);
+            handleViewChange();
+          }}
+          disabled={!gridView}
+        >
+          <ViewStreamIcon />
+        </Button>
+        <Button
+          onClick={() => {
+            setGridView(true);
+            handleViewChange();
+          }}
+          disabled={gridView}
+        >
+          <ViewModuleIcon />
+        </Button>
+      </ButtonGroup>
+      <ul className={`ProductsList${gridView ? ' flex-layout' : ''}`}>
+        {products.map((product, index) => (
+          <Product key={product.id} index={index} {...product} />
+        ))}
+      </ul>
+    </>
   );
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
-import { RootState } from '../redux/store';
 import LoginForm from '../components/LoginForm';
 import ProductsList from '../components/ProductsList';
 import CreateProductForm from '../components/CreateProductForm';
 
 const Home: React.FC = () => {
-  const { authenticated } = useSelector(
-    (state: RootState) => state.authentication
-  );
-
   return (
     <div className='Home'>
       <CreateProductForm />
